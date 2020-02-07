3f2e1aa2b86c966bbde9dfa69fd7be8897df5dac
ibrahim gad <hemagad@gmail.com>
2020-02-07 19:59:44 +0530

    Update main.py

--- Genetic Alg and SARIMA/main.py
+++ Genetic Alg and SARIMA/main.py
@@ -1,17 +1,23 @@
 """Entry point to evolving the neural network. Start here."""
+from multiprocessing import cpu_count
+from joblib import Parallel
+from joblib import delayed
+
+from __future__ import print_function
 import logging
 from optimizer import Optimizer
 from tqdm import tqdm
+import sys
 
 # Setup logging.
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%m/%d/%Y %I:%M:%S %p',
-    level=logging.DEBUG,
-    filename='log.txt'
+    level=logging.INFO#,
+    #filename='log.txt'
 )
 
-def train_networks(networks, dataset):
+def train_networks(networks, dataset, type_ser):
     """Train each network.
 
     Args:
@@ -20,7 +26,7 @@ def train_networks(networks, dataset):
     """
     pbar = tqdm(total=len(networks))
     for network in networks:
-        network.train(dataset)
+        network.train(dataset, type_ser)
         pbar.update(1)
     pbar.close()
 
@@ -36,11 +42,36 @@ def get_average_accuracy(networks):
     """
     total_accuracy = 0
     for network in networks:
-        total_accuracy += network.accuracy
+        if network.accuracy != -1 and network.accuracy != None:
+            total_accuracy += network.accuracy
+        else:
+            continue
 
     return total_accuracy / len(networks)
 
-def generate(generations, population, nn_param_choices, dataset):
+def get_min_mse(networks):
+    """Get the average accuracy for a group of networks/genomes.
+
+    Args:
+        networks (list): List of networks/genomes
+
+    Returns:
+        float: The average accuracy of a population of networks/genomes.
+
+    """
+    min_accuracy = 1000000
+    min_network  = None
+    for network in networks:
+        if network.accuracy != -1 and network.accuracy < min_accuracy:
+            min_accuracy = network.accuracy
+            min_network=network
+        else:
+            continue
+    return min_accuracy,min_network
+
+def generate(cfg, k, nn_param_choices, dataset, type_ser):
+    start_time = time.time()
+    generations, population = cfg
     """Generate a network with the genetic algorithm.
 
     Args:
@@ -50,35 +81,63 @@ def generate(generations, population, nn_param_choices, dataset):
         dataset (str): Dataset to use for training/evaluating
 
     """
+    logging.info("***Evolving %d generations with population %d***" % (generations, population))
+    logging.info("***generate(generations, population, nn_param_choices, dataset)***")
     optimizer = Optimizer(nn_param_choices)
+    #print("--1--")
     networks = optimizer.create_population(population)
-
+    #print("--2--")
+    min_networks=[]
     # Evolve the generation.
     for i in range(generations):
-        logging.info("***Doing generation %d of %d***" %
-                     (i + 1, generations))
-
+        logging.info("*** Now in generation %d of %d ***" %(i + 1, generations))
+        #print_networks(networks)
         # Train and get accuracy for networks.
-        train_networks(networks, dataset)
+        train_networks(networks, dataset, type_ser)
 
         # Get the average accuracy for this generation.
         average_accuracy = get_average_accuracy(networks)
 
+        min_mse,net =get_min_mse(networks)
+        #print_networks(net)
+        #net.print_network()
         # Print out the average accuracy each generation.
-        logging.info("Generation average: %.2f%%" % (average_accuracy * 100))
-        logging.info('-'*80)
+        logging.info("Generation average: %.2f" % (average_accuracy ))#* 100
+        logging.info('-'*80) #-----------
+        logging.info("Generation min_mse: %.2f" % (min_mse ))#* 100
+        logging.info('-'*80) #-----------
 
         # Evolve, except on the last iteration.
         if i != generations - 1:
             # Do the evolution.
             networks = optimizer.evolve(networks)
-
+        min_networks.append(net)
+        clear_output()
     # Sort our final population.
-    networks = sorted(networks, key=lambda x: x.accuracy, reverse=True)
+    # Sort our final population according to performance.
+    networks=[x for x in networks if x.accuracy !=-1 and x.accuracy != None]
+    networks = sorted(networks, key=lambda x: x.accuracy, reverse=False)
+    print("Generation done")
 
     # Print out the top 5 networks.
+    logging.info("top 5 networks Generation" )
+    logging.info('='*80) #-----------
     print_networks(networks[:5])
-
+    
+    elapsed_time_secs = time.time() - start_time
+    #pd.DataFrame(networks[:5]).to_csv(file_network, sep=',', encoding='utf-8', mode='a', header=True)
+    for n in range(0,5):
+        pos=return_pos(generations, population)
+        score_table.loc[pos+n,2:] = networks[n].param, networks[n].param_seasonal, networks[n].accuracy, elapsed_time_secs#timedelta(seconds=round(elapsed_time_secs))
+        #score_table.loc[pos+n,0] = timedelta(seconds=round(elapsed_time_secs))
+    score_table.to_csv(file_name, sep=',', encoding='utf-8', mode='a', header=True)
+    
+    logging.info("min networks Generation" )
+    logging.info('-'*80) #-----------
+    min_networks = sorted(min_networks, key=lambda x: x.accuracy, reverse=False)
+    print_networks(min_networks)
+    return networks
+    
 def print_networks(networks):
     """Print a list of networks.
 
@@ -86,28 +145,52 @@ def print_networks(networks):
         networks (list): The population of networks
 
     """
-    logging.info('-'*80)
+    logging.info('#'*80)
     for network in networks:
         network.print_network()
-
+        
+# grid search configs
+def grid_search(dataset, models_gen, nn_param_choices, type_ser, parallel=True):
+	scores = None
+	k=0
+	if parallel:
+    #print("$$$$$$$$$$$$$$$$$$$$ %s $$$$$$$$$$$$$$$$$"% (type_ser))
+    #networks = generate(generation, population, k, nn_param_choices, dataset, type_ser)
+    #k=k+5
+		# execute configs in parallel cfg, k, nn_param_choices, dataset, type_ser
+		executor = Parallel(n_jobs=cpu_count(), backend='multiprocessing')
+		#tasks = (delayed(generate)(generation, population, k, nn_param_choices, dataset, type_ser) for cfg in cfg_list)
+		networks = (delayed(generate)(cfg, k, nn_param_choices, dataset, type_ser) for cfg in models_gen)
+		scores   = executor(networks)
+	else:
+		scores = [score_model(data, n_test, cfg) for cfg in cfg_list]
+	# remove empty results
+	#scores = [r for r in scores if r[1] != None]
+	# sort configs by error, asc
+	#scores.sort(key=lambda tup: tup[1])
+	return scores
+    
 def main():
     """Evolve a network."""
-    generations = 10  # Number of times to evole the population.
-    population = 20  # Number of networks in each generation.
+    generations  = [100,200]  # Number of times to evole the population.[10, 20, 50, 100, 200]
+    populations  = [ 300, 400, 500]  # Number of networks in each generation. [10, 50, 100, 200, 300, 400, 500]
     dataset = 'cifar10'
-
+   
     nn_param_choices = {
-        'nb_neurons': [64, 128, 256, 512, 768, 1024],
-        'nb_layers': [1, 2, 3, 4],
-        'activation': ['relu', 'elu', 'tanh', 'sigmoid'],
-        'optimizer': ['rmsprop', 'adam', 'sgd', 'adagrad',
-                      'adadelta', 'adamax', 'nadam'],
+        'p_values' : range(0, 13),#[0, 1, 4, 6, 8, 10],
+        'd_values' : range(0, 2),#[0, 1, 2], #range(0, 3)
+        'q_values' : range(0, 13),#[0, 1, 2], #range(0, 3)
+        'sp_values': range(0, 13),#[0, 1, 4, 6, 8, 10],
+        'sd_values': range(0, 2),#[0, 1, 2],
+        'sq_values': range(0, 13) #[0, 1, 2]
     }
-
-    logging.info("***Evolving %d generations with population %d***" %
-                 (generations, population))
-
-    generate(generations, population, nn_param_choices, dataset)
-
-if __name__ == '__main__':
-    main()
+    type_ser ='normalized'
+    models_gen = list()
+    #for type_ser in ['normal', 'log', 'loglog']: #'normalized',
+    for generation in generations: #'normalized',
+        for population in populations:
+            cfg1 = [generation, population]
+            models_gen.append(cfg1)
+          
+    networks = grid_search(dataset, models_gen, nn_param_choices, type_ser)       
+    return networks
